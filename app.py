fc04ead5579eff9e2ed07cd7cf6c7d6f04a65a3e
James <james.ekasiba@student.moringaschool.com>
2024-02-17 21:28:13 +0300

    adding CORS and README.md

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask_migrate import Migrate
 from flask_restful import  Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from models import db
 from resources.user import Register, Login
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config["JWT_SECRET_KEY"] = "super-secret" 
-
+CORS(app)
 db.init_app(app)
 migrate = Migrate(app, db)
 api = Api(app)
